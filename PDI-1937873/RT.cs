07663a0fe3bdde02086a31a6dba94358a429234e
OhKaiitsKai <aymeealvarezv@gmail.com>
2023-05-25 00:51:07 -0600

    Detecta los rostros, falta el conteo.

--- PDI-1937873/RT.cs
+++ PDI-1937873/RT.cs
@@ -1,10 +1,11 @@
 ﻿using AForge.Video;
 using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
-using Emgu.CV;
-
 
 namespace PDI_1937873
 {
@@ -12,8 +13,8 @@ namespace PDI_1937873
     {
 
         //bool Webcam;
-        FilterInfoCollection filterInfoCollection;
-        VideoCaptureDevice videoCaptureDevice;
+        FilterInfoCollection filterInfoCollection; //
+        VideoCaptureDevice videoCaptureDevice; //C
         public RT()
         {
             InitializeComponent();
@@ -39,25 +40,61 @@ namespace PDI_1937873
         //   Webcam = false;
         //}
         //}
+        static readonly CascadeClassifier cascadeClassifier =
+            new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
         private void button1_Click(object sender, EventArgs e)
         {
-         WebCamClose();
+            //Al hacer clic en el botón, el combobox debe tener seleccionado la cámara
+            WebCamClose();
             videoCaptureDevice = new VideoCaptureDevice
                 (filterInfoCollection[comboBox1.SelectedIndex].MonikerString);
             videoCaptureDevice.NewFrame += videoCaptureDevice_NewFrame;
             videoCaptureDevice.Start();
             RT_ControlRemoved();
         }
+        private List<Rectangle> detectedFaces = new List<Rectangle>();
         private void videoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
-            if (pictureBox1.Image != null)
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImage = bitmap.ToImage<Bgr, byte>();
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+
+            // Clear the list of detected faces
+            detectedFaces.Clear();
+            // Store all detected faces
+            detectedFaces.AddRange(rectangles);
+
+            // Draw rectangles for all detected faces
+            using (Graphics graphics = Graphics.FromImage(bitmap))
             {
-                pictureBox1.Image.Dispose();
+                using (Pen pen = new Pen(Color.Red, 1))
+                {
+                    foreach (Rectangle rectangle in detectedFaces)
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
             }
-            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+            //foreach (Rectangle rectangle in rectangles)
+            //{
+            //  using (Graphics graphics = Graphics.FromImage(bitmap))
+            //{
+            //  using (Pen pen = new Pen(Color.Red, 1))
+            //{
+            //  graphics.DrawRectangle(pen, rectangle);
+            //}
+            //}
+            //}
+            pictureBox1.Image = bitmap;
+            //if (pictureBox1.Image != null)
+            //{
+            //  pictureBox1.Image.Dispose();
+            //}
+            //pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
         }
         private void RT_Load_1(object sender, EventArgs e)
         {
+            //Contenido del Combobox
             filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
             foreach (FilterInfo filterInfo in filterInfoCollection)
                 comboBox1.Items.Add(filterInfo.Name);
@@ -89,6 +126,7 @@ namespace PDI_1937873
         private void FiltrosToolStripMenuItem_Click(object sender, EventArgs e)
         {
             //Se desglosa Imágenes y Vídeos
+            //return 
         }
 
         private void ImágenesToolStripMenuItem_Click(object sender, EventArgs e)
@@ -122,7 +160,7 @@ namespace PDI_1937873
 
         private void textBox1_ReadOnlyChanged(object sender, EventArgs e)
         {
-
+            //Contador
         }
     }
 }
